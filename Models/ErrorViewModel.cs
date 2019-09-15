ebefba555a9da3ce760244bd80687a004a3504f9
lGodie <diegozapata1345@gmail.com>
2019-09-15 14:57:14 -0500

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NoticiaBackend.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
